a2e7feb1130c9a3af00e9e95a28c72fd51e04004
strawberryoars <devincdennis+1@gmail.com>
2023-11-23 09:33:32 -1000

    diffuse materials

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,23 @@ use ray::Ray;
 use sphere::Sphere;
 use vec3::Point3;
  
+fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
+    // If we've exceeded the ray bounce limit, no more light is gathered
+    if depth <= 0 {
+        return Color::new(0.0, 0.0, 0.0);
+    }
+
+    let mut rec = HitRecord::new();
+    if world.hit(r, 0.001, common::INFINITY, &mut rec) {
+        let direction = rec.normal + vec3::random_unit_vector();
+        return 0.5 * ray_color(&Ray::new(rec.p, direction), world, depth - 1);
+    }
+
+    let unit_direction = vec3::unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
     let a = r.direction().length_squared();
@@ -30,17 +47,6 @@ fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     }
 }
 
-fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
-    let mut rec = HitRecord::new();
-    if world.hit(r, 0.0, common::INFINITY, &mut rec) {
-        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
-    }
-
-    let unit_direction = vec3::unit_vector(r.direction());
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
-}
-
 fn main() {
     
     // Image
@@ -48,6 +54,7 @@ fn main() {
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
     const SAMPLES_PER_PIXEL: i32 = 100;
+    const MAX_DEPTH: i32 = 50;
 
     // World
  
@@ -57,15 +64,6 @@ fn main() {
 
     // Camera
     let cam = Camera::new();
-    // let viewport_height = 2.0;
-    // let viewport_width = ASPECT_RATIO * viewport_height;
-    // let focal_length = 1.0;
- 
-    // let origin = Point3::new(0.0, 0.0, 0.0);
-    // let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    // let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    // let lower_left_corner =
-    //     origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
     
     // Render
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -78,7 +76,7 @@ fn main() {
                 let u = (i as f64 + common::random_double()) / (IMAGE_WIDTH - 1) as f64;
                 let v = (j as f64 + common::random_double()) / (IMAGE_HEIGHT - 1) as f64;
                 let r = cam.get_ray(u, v);
-                pixel_color += ray_color(&r, &world);
+                pixel_color += ray_color(&r, &world, MAX_DEPTH);
             }
             color::write_color(&mut io::stdout(), pixel_color, SAMPLES_PER_PIXEL);
         }
